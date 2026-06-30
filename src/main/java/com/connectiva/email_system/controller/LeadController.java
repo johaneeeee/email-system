f98b4420225b01e162a60d8867a82ff4bba4535f
Mihanta Johane <johanemihanta@gmail.com>
2026-06-30 10:16:00 +0400

    Refactor LeadController and update email sending endpoint
    Refactored LeadController to use @Autowired for EmailService and changed endpoint to send campaigns.

--- src/main/java/com/connectiva/email_system/controller/LeadController.java
+++ src/main/java/com/connectiva/email_system/controller/LeadController.java
@@ -1,40 +1,20 @@
 package com.connectiva.email_system.controller;
 
-import com.connectiva.email_system.model.Lead;
 import com.connectiva.email_system.service.EmailService;
-import com.connectiva.email_system.service.LeadService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class LeadController {
 
-    private final LeadService leadService;
-    private final EmailService emailService;
+    @Autowired
+    private EmailService emailService;
 
-    public LeadController(LeadService leadService, EmailService emailService) {
-        this.leadService = leadService;
-        this.emailService = emailService;
-    }
-
-    // Test endpoint: see all leads
-    @GetMapping("/leads")
-    public List<Lead> getLeads() {
-        return leadService.getLeads();
-    }
-
-    // Trigger bulk email sending
-    @GetMapping("/send-emails")
-    public String sendEmails() {
-
-        List<Lead> leads = leadService.getLeads();
-
-        for (Lead lead : leads) {
-            emailService.sendEmail(lead);
-        }
-
-        return "Emails processed 🚀";
+    @PostMapping("/send")
+    public ResponseEntity<String> sendCampaign(@RequestParam String eventId) {
+        emailService.sendCampaign(eventId);
+        return ResponseEntity.ok("Campaign sent for event: " + eventId);
     }
 }
